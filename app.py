555fbddcf41b8db1d9dc6ec51904b4248cbc46e0
fallout <aror6916@stthomas.edu>
2022-12-01 17:08:17 -0600

    Working on actual data now

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import pandas as pd
 import os
 
 BASE_DIR = r'/Users/fallout/research/data/retail_db_json'
-table_name = 'order_items'
+table_name = 'orders'
+query = 'SELECT * FROM departments'
+"""
+Connection to the database
+"""
+conn = 'postgresql://retail_user:itversity@localhost:5432/retail_db'
 
 
 def main():
@@ -22,17 +27,39 @@ def main():
         print(f'Number of records in chunk with index {idx} is {df.shape[0]}')
 
 
-def sql_table():
-    users_list = [
-        {'user_first_name': 'Scott', 'user_last_name': 'Tiger'},
-        {'user_first_name': 'Donald', 'user_last_name': 'Duck'}
-    ]
-    df = pd.DataFrame(users_list)
-    conn = 'postgresql://retail_user:itversity@localhost:5432/retail_db'
-    df.to_sql('users', conn, if_exists='append', index=False)
-    print(pd.read_sql('SELECT * FROM users', conn))
+def load_table():
+    pd.read_sql('SELECT * FROM departments', conn)
+
+    file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
+    fp = f'{BASE_DIR}/{table_name}/{file_name}'
+    df = pd.read_json(fp, lines=True)
+    df.to_sql(table_name, conn, if_exists='append', index=False)
+
+
+def read_table():
+    df = pd.read_sql(query, conn)
+    print(df.head())
+
+def p_orders_table():
+    """
+    Read data from files using read_json with chunksize set to 1000.
+    Iterate through JSON Reader object.
+    Load Dataframe related to each chunk into the target table.
+    """
+    file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
+    fp = f'{BASE_DIR}/{table_name}/{file_name}'
+    json_reader = pd.read_json(fp, lines=True, chunksize=1000)
+
+    for df in json_reader
+        min_key = df['order_id'].min()
+        max_key = df['order_id'].max()
+        df.to_sql(table_name, conn, if_exists='append', index=False)
+        print(f'Processed {table_name} with in the range of {min_key} and {max_key}')
+
 
 
 if __name__ == '__main__':
-    main()
-    sql_table()
+    # main()
+    #sql_table()
+    #read_table()
+    p_orders_table
