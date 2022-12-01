0f6133d2ab2041aa20dd1fe112a21731fc2dccba
fallout <aror6916@stthomas.edu>
2022-12-01 16:40:55 -0600

    Initial Commit

--- app.py
+++ app.py
@@ -22,5 +22,17 @@ def main():
         print(f'Number of records in chunk with index {idx} is {df.shape[0]}')
 
 
+def sql_table():
+    users_list = [
+        {'user_first_name': 'Scott', 'user_last_name': 'Tiger'},
+        {'user_first_name': 'Donald', 'user_last_name': 'Duck'}
+    ]
+    df = pd.DataFrame(users_list)
+    conn = 'postgresql://retail_user:itversity@localhost:5432/retail_db'
+    df.to_sql('users', conn, if_exists='append', index=False)
+    print(pd.read_sql('SELECT * FROM users', conn))
+
+
 if __name__ == '__main__':
     main()
+    sql_table()
