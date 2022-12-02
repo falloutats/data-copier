f8be1b8d90829ad23365a0c841ac41738e20fd49
fallout <aror6916@stthomas.edu>
2022-12-02 16:23:41 -0600

    final_app

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import os
+import sys
 import pandas as pd
 from read import get_json_reader
 from write import load_db_table
 
 def main():
     BASE_DIR = os.environ.get('BASE_DIR')
-    table_name = os.environ.get('TABLE_NAME')
+    table_names = sys.argv[1].split(',')
+
     configs = dict(os.environ.items())
     conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+    for table_name in table_names:
+        process_table(BASE_DIR, conn, table_name)
+
+
+def process_table(BASE_DIR, conn, table_name):
     json_reader = get_json_reader(BASE_DIR, table_name)
     for df in json_reader:
         load_db_table(df, conn, table_name, df.columns[0])
